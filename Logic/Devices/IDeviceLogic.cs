24df90fbf04e6a16397027bbad790c6bc5cb41f1
lars <larsvaagenes@gmail.com>
2019-11-12 15:34:46 +0100

    s

--- Logic/Devices/IDeviceLogic.cs
+++ Logic/Devices/IDeviceLogic.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections.Generic;
+using IoTPortal.Model;
 
-namespace Logic.Device
+namespace Logic.Devices
 {
     public interface IDeviceLogic
     {
         bool RemoveDevice(int deviceId);
-        void SaveDevice(IoTPortal.Model.Device device);
-        List<IoTPortal.Model.Device> GetDevicesFromUser(int userId, bool checkPublished = false, bool published = true);
-        List<IoTPortal.Model.Device> GetDevices(bool checkPublished = false, bool published = true);
-        IoTPortal.Model.Device GetDevice(int deviceId, bool checkPublished = false, bool published = true);
-        IoTPortal.Model.Device GetDevice(string deviceName, bool checkPublished = false, bool published = true);
-        List<IoTPortal.Model.Device> GetDevices(string nameContains, bool checkPublished = false, bool published = true);
+        void SaveDevice(Device device);
+        List<Device> GetDevicesFromUser(int userId, bool checkPublished = false, bool published = true);
+        List<Device> GetDevices(bool checkPublished = false, bool published = true);
+        Device GetDevice(int deviceId, bool checkPublished = false, bool published = true);
+        Device GetDevice(string deviceName, bool checkPublished = false, bool published = true);
+        List<Device> GetDevices(string nameContains, bool checkPublished = false, bool published = true);
     }
 }
